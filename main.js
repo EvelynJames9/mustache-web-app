fb3e29fc4cb0605b3a448a9d81b4258175e3cacd
EvelynJames9 <98927398+EvelynJames9@users.noreply.github.com>
2022-07-19 10:16:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
@@ -17,6 +21,8 @@ function gotPoses(results){
         console.log(results);
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;  
     }
 }
 
@@ -26,6 +32,10 @@ function modelLoaded() {
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255, 0, 0)
+    //circle(noseX, noseY, 20);
+    image(clown_nose, noseX-15, noseY+5, 30, 30);
 }
 
 function take_snapshot(){
